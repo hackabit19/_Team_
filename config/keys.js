8b50c376ac0d97fcd60f059736e6a8f0cd98ed7b
lucifer-7 <ishanbakshic1s8k8@gmail.com>
2019-10-19 15:05:22 +0530

    with keys

---
+++ config/keys.js
@@ -0,0 +1,20 @@
+// added to gitignore
+
+module.exports={
+    google:{
+        clientID:'762793500286-24sfv9mdma2htet3t605k3up5ml1ghdh.apps.googleusercontent.com',
+        clientSecret:'0aFVQj6M5ESOuK-Kf87W5GQf'
+    },
+    mongodb:{
+        dbURI:'mongodb+srv://lucifer_7:ishan123@surakshit-hslln.mongodb.net/admin?retryWrites=true&w=majority'
+    },
+    cookieSession:{
+        cookieKey: 'sabjhaansahai'
+    },
+    ably:{
+        ablyKey: 'FzGgmg.Z07Qbw:uMa6eafC95XUl-ZJ'
+    },
+    geocoding:{
+        geocodingKey: 'AIzaSyAq1q2lzF5wRQZyV-JcA_eKCqWSExmPbFE'
+    }
+};
